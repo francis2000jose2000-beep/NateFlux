425efaa657c605c2060e742630626729419137fb
Francisco Rodrigues <francis2000.jose2000@gmail.com>
2026-03-12 03:10:14 +0000

    Add test resource to fix empty configuration

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -8,9 +8,10 @@ terraform {
   }
 }
 
-# This is a "dummy" resource that just proves the connection works.
-resource "null_resource" "pipeline_test" {
+# This is a "fake" resource that just proves the connection works.
+# It doesn't cost money and doesn't need the CLI to run.
+resource "null_resource" "orchestration_test" {
   provisioner "local-exec" {
-    command = "echo 'DevSecOps Pipeline Triggered Successfully!'"
+    command = "echo 'DevSecOps Pipeline Successfully Triggered!'"
   }
 }

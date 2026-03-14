378917e845389e1dc3a87b952a5dc15bb0b25fbe
Francisco Rodrigues <francis2000.jose2000@gmail.com>
2026-03-14 18:59:12 +0000

    fix: remove solid backgrounds to reveal LiquidChrome animation

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-transparent`}
       >
         <div className="fixed inset-0 -z-10 h-screen w-screen">
           <LiquidChrome 
